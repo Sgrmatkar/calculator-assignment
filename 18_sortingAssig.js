b8b5eb6b3901e5bb19c3ff018852ac3e1155aede
Sgrmatkar <sgrmatkar@gmail.com>
2024-01-26 20:09:29 +0530

    sorting assignment
    sorting assignment

---
+++ 18_sortingAssig.js
@@ -0,0 +1,13 @@
+let arrayRollNumbers = [13, 45, 56, 11, 32, 45, 109, 799, 56, 45];
+console.log(`Given array is: ${arrayRollNumbers}`);
+console.log(`======================================STEP-01======================================`);
+console.log(`Reverse array is:`);
+console.log(arrayRollNumbers);
+console.log(`======================================STEP-02======================================`);
+console.log(`Sorting array with no arg.`);
+let arrayRollNumbers1 = [13, 45, 56, 11, 32, 45, 109, 799, 56, 45];
+arrayRollNumbers1.sort();
+console.log(arrayRollNumbers1);
+
+console.log(`======================================STEP-03======================================`);
+console.log(`Sorting array in ascending order`);
